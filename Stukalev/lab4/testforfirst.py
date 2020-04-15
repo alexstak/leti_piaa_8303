7c5d4b95aadf6da50637064f038fdf29fce17107
alexstak <42896766+alexstak@users.noreply.github.com>
2020-04-15 23:13:37 +0300

    fix2

--- Stukalev/lab4/testforfirst.py
+++ Stukalev/lab4/testforfirst.py
@@ -8,4 +8,4 @@ for i in range(len(tests)):
     testPath = DIRECTORY + "/" + tests[i]
     print("Test №", i+1, ": ", testPath, sep="")
     print(*open(testPath, "r").readlines(), sep="")
-    os.system("./lab2 < " + testPath)
+    os.system("./lab4 < " + testPath)
